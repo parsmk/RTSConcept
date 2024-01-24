c6da9a14dac3bb2da4e3fdb14cd847de11b76563
Parsa <p.mohtashami.k@gmail.com>
2024-01-24 15:58:35 -0800

    Initial InteractableObject commit

---
+++ Assets/Scripts/InteractableObjects/InteractableObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class InteractableObject {
+    public ObjectStats objectStats;
+
+    public bool PerformAction() {
+        return true;
+    }
+}
---
+++ Assets/Scripts/InteractableObjects/ObjectStats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ObjectStats: ScriptableObject {
+    public int maxHealth;
+    public int health;
+
+    public int attackPower;
+    public int attackSpeed;
+
+    public int foodCost = 0;
+    public int woodCost = 0;
+    public int goldCost = 0;
+    public int stoneCost = 0;
+}
