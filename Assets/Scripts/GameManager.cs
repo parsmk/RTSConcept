4851cd040590c26fdf768bb31ad66c85c4d2fb28
Parsa <p.mohtashami.k@gmail.com>
2024-09-17 12:19:29 -0700

    Laptop Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private static Resource[] resources;
+
+    public Resource[] GetResources() {
+        if (resources == null) {
+            JsonUtility.FromJson(Resources.Load<Resource>("Data/resources"));
+        }
+
+        return resources;
+    }
+}
---
+++ Assets/Scripts/Terrain/Resource.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Resource{
+    public string name;
+    public int resourceCount;
+    public float maxNoiseBound;
+    public float minNoiseBound;
+}
